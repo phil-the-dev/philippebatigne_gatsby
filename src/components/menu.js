d920dad934c38a7bea6740c90f703ba0d295ba79
Philippe Batigne <pbatigne@gmail.com>
2019-04-09 23:28:07 -0400

    fixed the sliding of the menu!

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Collapse from '@kunukn/react-collapse'
+import { SlideDown } from 'react-slidedown'
+import 'react-slidedown/lib/slidedown.css'
 
 export default class Menu extends React.Component {
   constructor(props) {
@@ -19,31 +20,25 @@ export default class Menu extends React.Component {
 
   render() {
     let menuToggleClass = "menu-toggle " + (this.state.isClicked ? 'is-clicked' : '');
-    let isOpen = this.state.isClicked;
     return (
       <div className="row">
-        <div class="top-bar">
-
-          <a class={menuToggleClass} href="#" onClick={() => this.menuClick()}>
+        <div className="top-bar">
+          <a className={menuToggleClass} href="#" onClick={() => this.menuClick()}>
             <span>Menu</span>
           </a>
-
-          <div class="logo">
-            <a href="index.html">Home</a>
-          </div>
-
           <nav id="main-nav-wrap">
-            <Collapse isOpen={this.state.isClicked}>
-              <ul class="main-navigation">
-                <li class="current"><a class="smoothscroll" href="#intro" title="">Home</a></li>
-                <li><a class="smoothscroll" href="#about" title="">About</a></li>
-                <li><a class="smoothscroll" href="#services" title="">Why Me?</a></li>
-                <li><a class="smoothscroll" href="#resume" title="">Resume</a></li>
-                <li><a class="smoothscroll" href="#portfolio" title="">Portfolio</a></li>
-                <li><a class="smoothscroll" href="#contact" title="">Contact</a></li>
+            <SlideDown className={'my-dropdown-slidedown'} closed={!this.state.isClicked}>
+              <ul className="main-navigation">
+                <li className="current"><a className="smoothscroll" href="#intro" title="">Home</a></li>
+                <li><a className="smoothscroll" href="#about" title="">About</a></li>
+                <li><a className="smoothscroll" href="#services" title="">Why Me?</a></li>
+                <li><a className="smoothscroll" href="#resume" title="">Resume</a></li>
+                <li><a className="smoothscroll" href="#portfolio" title="">Portfolio</a></li>
+                <li><a className="smoothscroll" href="#contact" title="">Contact</a></li>
               </ul>
-            </Collapse>
+            </SlideDown>
           </nav>
+
         </div>
       </div >
     );
