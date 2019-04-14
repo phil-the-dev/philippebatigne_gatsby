80c2b51b194a0febc76f1757fbeb99b58b48aea8
Philippe Batigne <pbatigne@gmail.com>
2019-04-14 00:09:54 -0400

    fixing small menu bugs

--- src/components/menu.js
+++ src/components/menu.js
@@ -7,37 +7,43 @@ export default class Menu extends React.Component {
   constructor(props) {
     super(props);
 
-
     this.state = {
-      isClicked: false,
+      isMenuOpen: false,
     }
   }
 
-  menuClick() {
+  menuClick(event) {
     this.setState(state => ({
-      isClicked: !this.state.isClicked
+      isMenuOpen: !this.state.isMenuOpen
+    }))
+    event.preventDefault();
+  }
+
+  linkClick() {
+    this.setState(state => ({
+      isMenuOpen: false
     }))
   }
 
   render() {
-    let menuToggleClass = "menu-toggle " + (this.state.isClicked ? 'is-clicked' : '');
+    let menuToggleClass = "menu-toggle " + (this.state.isMenuOpen ? 'is-clicked' : '');
     return (
       <div className="row">
         <div className="top-bar">
-          <a className={menuToggleClass} href="#" onClick={() => this.menuClick()}>
+          <a className={menuToggleClass} href="#" onClick={(e) => this.menuClick(e)}>
             <span>Menu</span>
           </a>
           <nav id="main-nav-wrap">
-            <SlideDown className={'my-dropdown-slidedown'} closed={!this.state.isClicked}>
+            <SlideDown className={'my-dropdown-slidedown'} closed={!this.state.isMenuOpen}>
               <ul className="main-navigation">
                 <li className="current">
-                  <AnchorLink href='#intro'>Home</AnchorLink>
+                  <AnchorLink onClick={() => this.linkClick()} href='#intro'>Home</AnchorLink>
                 </li>
-                <li><AnchorLink href='#about'>About</AnchorLink></li>
-                <li><AnchorLink href='#services'>Why Me?</AnchorLink></li>
-                <li><AnchorLink href='#resume'>Resume</AnchorLink></li>
-                <li><AnchorLink href='#portfolio'>Portfolio</AnchorLink></li>
-                <li><AnchorLink href='#contact'>Contact</AnchorLink></li>
+                <li><AnchorLink onClick={() => this.linkClick()} href='#about'>About</AnchorLink></li>
+                <li><AnchorLink onClick={() => this.linkClick()} href='#services'>Why Me?</AnchorLink></li>
+                <li><AnchorLink onClick={() => this.linkClick()} href='#resume'>Resume</AnchorLink></li>
+                <li><AnchorLink onClick={() => this.linkClick()} href='#portfolio'>Portfolio</AnchorLink></li>
+                <li><AnchorLink onClick={() => this.linkClick()} href='#contact'>Contact</AnchorLink></li>
               </ul>
             </SlideDown>
           </nav>
