9bd60afe90dfe8469c266f3159e493841dc5af7d
Philippe Batigne <pbatigne@gmail.com>
2019-07-24 18:42:37 -0400

    updating title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: `/assets/js`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `PhilippeBatigne.com`,
+    description: `Homepage of Philippe Batigne.`,
+    author: `@phil_the_dev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
