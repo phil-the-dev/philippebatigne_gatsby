148315d0808525ad8cbcb5d0fecf0ea9680600b3
Philippe Batigne <pbatigne@gmail.com>
2019-07-29 00:41:06 -0400

    add gatsby icon, fixing link

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,15 @@ import ServicesSection from "../components/section/services"
 import ResumeSection from "../components/section/resume"
 import ContactSection from "../components/section/contact"
 import '../assets/scss/philippebatigne.scss'
+import Icon from '../images/gatsby-icon.png'
+
 import { Link } from "gatsby";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Banner />
-    <div className="gatsby-ribbon">HTML site proudly converted to <Link to="//www.gatsbyjs.org/">Gatsby</Link></div>
+    <div className="gatsby-ribbon">HTML site proudly converted to <img src={Icon} style={{ height: '17px', marginBottom: '-3px' }} /><a href="//www.gatsbyjs.org/">Gatsby</a></div>
     <AboutSection />
     <ProgressSection />
     <ServicesSection />
