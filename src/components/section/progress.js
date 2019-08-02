2b9efae03546491c6650cac7de5f46c1919a6b13
Philippe Batigne <pbatigne@gmail.com>
2019-08-01 22:45:37 -0400

    collapsing columns on mobile

--- src/components/section/progress.js
+++ src/components/section/progress.js
@@ -5,13 +5,13 @@ const ProgressSection = () => (
   <section id="cta" className="grey-section">
     <h2 className="h01">What I'm learning right now</h2>
     <div className="row">
-      <div className="col-six">
+      <div className="col-six tab-full">
         <p className="lead">
           <a href="https://facebook.github.io/react/" rel="noopener noreferrer" target="_blank"><img src={react_logo} alt="" style={{ height: `100px` }} /></a> <br />
           Currently learning React so I can decouple my Rails apps and separate client-side functionality, reduce server calls, speed up page loads, and create more efficient code.
         </p>
       </div>
-      <div className="col-six">
+      <div className="col-six tab-full">
         <h1 style={{ fontSize: "70px", margin: "0px" }}>UI/UX Design</h1>
         <p className="lead" style={{ marginTop: "18px" }}>
           I've always had a designer or guidelines for my projects, but I've always found myself a little lost when it comes to making designs and user interfaces. I want to be more of a designer and be able to be a self suffient web developer.
